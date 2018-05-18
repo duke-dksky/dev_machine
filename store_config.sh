16251ec0ea9c709a6f5540b1b87b2dbc42e5e381
duke.yang <duke.yang@ucloud.cn>
2018-05-18 14:56:27 +0800

    增加配置文件

---
+++ store_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mydir=$(dirname "${BASH_SOURCE[0]}")
+
+cp "${HOME}/.tmux.conf" "${mydir}/tmux_config"
+cp "${HOME}/.vimrc" "${mydir}/vim_config"
+cp "${HOME}/.gitconfig" "${mydir}/git_config"
+cp "${HOME}/.bash_profile" "${mydir}/bash_config"
+
